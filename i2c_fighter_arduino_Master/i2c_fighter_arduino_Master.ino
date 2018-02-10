7ff7713efb669562f50775988e86e8a02888274b
kwdi <kostas.droid@gmail.com>
2018-02-10 18:14:14 +0100

    i2c works master and 2 slaves

--- i2c_fighter_arduino_Master/i2c_fighter_arduino_Master.ino
+++ i2c_fighter_arduino_Master/i2c_fighter_arduino_Master.ino
@@ -4,6 +4,7 @@
 #define PAPER 1
 #define SCISSORS 2
 #define ROCK 3
+#define NUM_CONTESTANTS (2+1)
 
 int contestant1;
 int contestant2;
@@ -21,53 +22,73 @@ void setup() {
 byte x = 0;
 
 void ContGen(){
-  
-  contestant1=random(1,3);
-  contestant2=random(1,3);
+  contestant1=random(1,NUM_CONTESTANTS);
+  contestant2=random(1,NUM_CONTESTANTS);
   while(contestant1==contestant2){
-    contestant2=random(1,2);
-    Serial.println("c1");
-    Serial.print(contestant1);
-    Serial.println("c2");
-    Serial.print(contestant2);
-    Serial.println("GEN WHILE");
+    contestant2=random(1,NUM_CONTESTANTS);
+    Serial.print("c1 ");
+    Serial.println(contestant1);
+    Serial.print("c2 ");
+    Serial.println(contestant2);
   }
   
 
 }
 void StartGame(){
   Wire.requestFrom(contestant1, 1);    // request 6 bytes from slave device #8
-
   while (Wire.available()) { // slave may send less than requested
     contResp1 = Wire.read(); // receive a byte as character
   }
 
+    Serial.print(contestant1);
+    Serial.print(" chose ");
+    switch (contResp1){
+      case 1: 
+        Serial.println("paper"); 
+        break;
+      case 2: 
+        Serial.println("scissors"); 
+        break;
+      case 3: 
+        Serial.println("rock");
+        break;
+    }
   Wire.requestFrom(contestant2, 1);    // request 6 bytes from slave device #8
 
   while (Wire.available()) { // slave may send less than requested
     contResp2 = Wire.read(); // receive a byte as character
   }
-  delay(100);
+
+    Serial.print(contestant2);
+    Serial.print(" chose ");
+    switch (contResp2){
+      case 1: 
+        Serial.println("paper"); 
+        break;
+      case 2: 
+        Serial.println("scissors"); 
+        break;
+      case 3: 
+        Serial.println("rock");
+        break;
+    }
+  
+  //delay(100);
 }
 
 void loop() {
-    Serial.println("LOOP START");
+  Serial.println("LOOP START");
 
   digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Generating Players ");
   ContGen();
-  Serial.println("GEN NUM");
+  Serial.println("Requesting weapons ");
   StartGame();
-    Serial.println("GAMESTART");
-
-  
-  while(contResp1==contResp2){
-      StartGame();  
+  while(contResp1==contResp2 && contResp1!=0 ){
+      Serial.println("Same Weapons chosen, REDO!");
+      StartGame();
   }
-    Serial.println(contestant2);
-    Serial.println(contestant1);
-
   
-
   switch(contResp1){
     case PAPER:
        if(contResp2 == SCISSORS) winner=contestant2;
@@ -80,15 +101,17 @@ void loop() {
     case ROCK:
        if(contResp2 == PAPER) winner=contestant2;
        else winner = contestant1;
-       break;   
+       break;
+    default:
+      break;   
   }
 
+  
   Wire.beginTransmission(winner);
   Wire.write(7); //7 defines a lucky number so we send that as a response to the winner
   digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("winner");
-  Serial.print(winner);
+  Serial.print("----------------------Winner is : ");
+  Serial.println(winner);
   delay(500);
-  
-
+ 
 }
--- i2c_fighter_arduino_uno_slave1/i2c_fighter_arduino_uno_slave1.ino
+++ i2c_fighter_arduino_uno_slave1/i2c_fighter_arduino_uno_slave1.ino
@@ -1,15 +1,26 @@
 #include <Wire.h>
+#define OWN_ADDRESS 1
 
+int winResp = 0; 
 void setup() {
-  Wire.begin(8);                // join i2c bus with address #8
+  Wire.begin(OWN_ADDRESS);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(requestEvent);
   Serial.begin(9600);           // start serial for output
   pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
 void loop() {
-  delay(100);
+  //TODO: Respond to REQ 
+  //Wait for winner or loop
+  delay(200);  
+
+}
+
+
+void requestEvent(){
+  Wire.write(random(1,4));
 }
 
 // function that executes whenever data is received from master
@@ -18,12 +29,16 @@ void receiveEvent(int howMany) {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)   
 
   while (1 < Wire.available()) { // loop through all but the last
-    char c = Wire.read(); // receive byte as a character
+    int c = Wire.read(); // receive byte as a character
     Serial.print(c);         // print the character
   }
   int x = Wire.read();    // receive byte as an integer
-  Serial.println("slave");
-  Serial.println(x);         // print the integer
+  if(x == 7){
+    Serial.print("slave ");
+    Serial.print((int)OWN_ADDRESS);
+    Serial.print(" WON!!!"); 
+    Serial.println(x);         // print the integer
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-
+  }
+  //TODO: WINNER SEQUENCEs!
 }
